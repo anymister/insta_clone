3dd7be9a2fc23617b9b81ffbcacf7a746c1d8c2c
anis mezrag <35765611+anymister@users.noreply.github.com>
2024-01-04 15:42:36 +0100

    Update SidebarItems.jsx

--- src/components/Sidebar/SidebarItems.jsx
+++ src/components/Sidebar/SidebarItems.jsx
@@ -3,17 +3,19 @@ import Home from "./Home";
 import Notifications from "./Notifications";
 import ProfileLink from "./ProfileLink";
 import Search from "./Search";
+import ScanPage from "./ScanPage";
 
 const SidebarItems = () => {
-	return (
-		<>
-			<Home />
-			<Search />
-			<Notifications />
-			<CreatePost />
-			<ProfileLink />
-		</>
-	);
+  return (
+    <>
+      <Home />
+      <Search />
+      <Notifications />
+      <CreatePost />
+      <ProfileLink />
+      <a href="/scan">Scan</a>
+    </>
+  );
 };
 
 export default SidebarItems;
