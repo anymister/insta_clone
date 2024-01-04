636976fcf5ee4d65a509767cd8a473b53b371cfe
anis mezrag <35765611+anymister@users.noreply.github.com>
2024-01-04 16:31:37 +0100

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,46 +1,20 @@
-import { Link, Tooltip } from "@chakra-ui/react";
-import { AiFillHome } from "react-icons/ai";
-import { Link as RouterLink } from "react-router-dom";
-import ScanPage from "./ScanPage";
+import { Box, Container, Flex } from "@chakra-ui/react";
+import FeedPosts from "../../components/FeedPosts/FeedPosts";
+import SuggestedUsers from "../../components/SuggestedUsers/SuggestedUsers";
 
-const Home = () => {
-  return (
-    <>
-      <Tooltip
-        hasArrow
-        label={"Home"}
-        placement="right"
-        ml={1}
-        openDelay={500}
-        display={{ base: "block", md: "none" }}
-      >
-        <Link
-          display={"flex"}
-          to={"/"}
-          as={RouterLink}
-          alignItems="center"
-          gap={4}
-          _hover={{ bg: "whiteAlpha.400" }}
-          borderRadius={6}
-          p={2}
-          w={{ base: 10, md: "full" }}
-          justifyContent={{ base: "center", md: "flex-start" }}
-        >
-          <AiFillHome size={25} />
-          <Box display={{ base: "none", md: "block" }}>Home</Box>
-        </Link>
-      </Tooltip>
-      <a
-        href="/scan"
-        onClick={() => {
-          // Handle the navigation to the ScanPage
-          alert("Navigating to ScanPage");
-        }}
-      >
-        <Box ml={3}>Scan</Box>
-      </a>
-    </>
-  );
+const HomePage = () => {
+	return (
+		<Container maxW={"container.lg"}>
+			<Flex gap={20}>
+				<Box flex={2} py={10}>
+					<FeedPosts />
+				</Box>
+				<Box flex={3} mr={20} display={{ base: "none", lg: "block" }} maxW={"300px"}>
+					<SuggestedUsers />
+				</Box>
+			</Flex>
+		</Container>
+	);
 };
 
-export default Home;
+export default HomePage;
