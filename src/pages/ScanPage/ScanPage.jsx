c2ad231ce5811214a5ca303fcd5510e145de3a9d
anis mezrag <35765611+anymister@users.noreply.github.com>
2024-01-04 19:30:36 +0100

    Add files via upload

--- src/pages/ScanPage/ScanPage.jsx
+++ src/pages/ScanPage/ScanPage.jsx
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from "react";
-import { Container, Flex, Skeleton, SkeletonCircle, Text, VStack } from "@chakra-ui/react";
-import BarcodeScanner from "react-barcode-scanner";
-import Button from "../../components/Button";
-import NFCReader from "react-nfc";
+import React, { useState } from 'react';
+import { Container, Flex, Skeleton, SkeletonCircle, Text, VStack } from '@chakra-ui/react';
+import BarcodeScanner from 'react-barcode-scanner';
+import Button from '../../components/Button';
 
 const ScanPage = () => {
   const [scanResult, setScanResult] = useState('');
@@ -11,17 +10,6 @@ const ScanPage = () => {
     const scanData = event.detail.data;
     setScanResult(scanData);
   };
-
-  useEffect(() => {
-    const scanner = new NFCReader();
-    scanner.on('scan', handleScan);
-    scanner.start();
-
-    // Stop the scanner when the component is unmounted
-    return () => {
-      scanner.stop();
-    };
-  }, []);
 
   return (
     <Container maxW='container.lg' py={5}>
@@ -32,14 +20,15 @@ const ScanPage = () => {
 
         {scanResult && (
           <Flex flexDirection='column'>
-            <Text fontSize={'2xl'}>{scanResult.id}</Text>
-            <Text fontSize={'2xl'}>{scanResult.type}</Text>
-            <Text fontSize={'2xl'}>{scanResult.payload}</Text>
+            <Text fontSize={'2xl'}>{scanResult}</Text>
           </Flex>
         )}
 
         <Button variant='primary' onClick={() => {
-          const scanner = new NFCReader();
+          const scanner = new BarcodeScanner({
+            render: true,
+            // Set the desired options for your barcode scanner
+          });
           scanner.on('scan', handleScan);
           scanner.start();
         }}>Scan</Button>
