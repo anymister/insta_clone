d5bba06876db9e30b7b2706737707cec1e708a91
anis mezrag <35765611+anymister@users.noreply.github.com>
2024-01-04 16:34:27 +0100

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,20 +1,31 @@
-import { Box, Container, Flex } from "@chakra-ui/react";
+import { Box, Container, Flex, Button } from "@chakra-ui/react";
 import FeedPosts from "../../components/FeedPosts/FeedPosts";
 import SuggestedUsers from "../../components/SuggestedUsers/SuggestedUsers";
+import ScanPage from "./ScanPage";
 
 const HomePage = () => {
-	return (
-		<Container maxW={"container.lg"}>
-			<Flex gap={20}>
-				<Box flex={2} py={10}>
-					<FeedPosts />
-				</Box>
-				<Box flex={3} mr={20} display={{ base: "none", lg: "block" }} maxW={"300px"}>
-					<SuggestedUsers />
-				</Box>
-			</Flex>
-		</Container>
-	);
+  return (
+    <Container maxW={"container.lg"}>
+      <Flex gap={20}>
+        <Box flex={2} py={10}>
+          <FeedPosts />
+        </Box>
+        <Box flex={3} mr={20} display={{ base: "none", lg: "block" }} maxW={"300px"}>
+          <SuggestedUsers />
+        </Box>
+        <Button
+          variant="primary"
+          as="button"
+          onClick={() => {
+            // Navigate to the ScanPage
+            alert("Navigating to ScanPage");
+          }}
+        >
+          Scan
+        </Button>
+      </Flex>
+    </Container>
+  );
 };
 
 export default HomePage;
